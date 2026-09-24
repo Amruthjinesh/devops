2b1ccd079b8130598da34607a6429a0a075b8eef
Ubuntu <ubuntu@ip-172-31-1-182.ap-south-1.compute.internal>
2026-09-24 09:34:35 +0000

    made changes to monitor

--- monitor.sh
+++ monitor.sh
@@ -22,3 +22,23 @@ uname -a
 echo ""
 echo "CPU Usage:"
 top -bn1 | grep "Cpu(s)"
+echo ""
+echo "Disk Alert Check:"
+
+DISK=$(df / | awk 'NR==2 {print $5}' | tr -d '%')
+
+if [ "$DISK" -ge 80 ]; then
+    echo "WARNING: Disk usage is ${DISK}%"
+else
+    echo "OK: Disk usage is ${DISK}%"
+fi
+echo ""
+echo "Memory Alert Check:"
+
+MEM=$(free | awk '/Mem:/ {printf "%.0f", $3/$2 * 100}')
+
+if [ "$MEM" -ge 80 ]; then
+    echo "WARNING: Memory usage is ${MEM}%"
+else
+    echo "OK: Memory usage is ${MEM}%"
+fi
